25cd5d4defac988432966c230be1a909eac6a63a
jeffrey-fabian <jfabian@rsglab.com>
2020-09-25 19:30:25 -0400

    refactor footer adblock checker

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,40 +7,7 @@ import Button from "@material-ui/core/Button";
 
 import Link from "../util/Link";
 
-// Determines if the user is likely using an ad block extension
-// source: https://davidwalsh.name/detect-ad-blocker
-async function checkAdBlocker() {
-  // Used to cache the result
-  let isBlocked;
 
-  async function tryRequest() {
-    try {
-      return fetch(
-        new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
-          method: 'HEAD',
-          mode: 'no-cors'
-        }))
-        .then(function (response) {
-          // Google Ads request succeeded, so likely no ad blocker
-          isBlocked = false;
-          return isBlocked;
-        }).catch(function (e) {
-          // Request failed, likely due to ad blocker
-          isBlocked = true;
-          return isBlocked;
-        });
-    } catch (error) {
-      // fetch API error; possible fetch not supported (old browser)
-      // Marking as a blocker since there was an error and so
-      // we can prevent continued requests when this function is run
-      console.log(error);
-      isBlocked = true;
-      return isBlocked;
-    }
-  }
-
-  return isBlocked !== undefined ? isBlocked : await tryRequest();
-}
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -102,9 +69,43 @@ export default function Footer({ children }) {
   const classes = useStyles();
   const [isAdblock, setIsAdblock] = useState(true);
 
-  useEffect(async () => {
-    const usingBlocker = await checkAdBlocker();
-    setIsAdblock(usingBlocker);
+  useEffect(() => {
+    // Determines if the user is likely using an ad block extension
+    // source: https://davidwalsh.name/detect-ad-blocker
+    async function checkAdBlocker() {
+      // Used to cache the result
+      let isBlocked;
+
+      async function tryRequest() {
+        try {
+          return fetch(
+            new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
+              method: 'HEAD',
+              mode: 'no-cors'
+            }))
+            .then(function (response) {
+              // Google Ads request succeeded, so likely no ad blocker
+              isBlocked = false;
+              return isBlocked;
+            }).catch(function (e) {
+              // Request failed, likely due to ad blocker
+              isBlocked = true;
+              return isBlocked;
+            });
+        } catch (error) {
+          // fetch API error; possible fetch not supported (old browser)
+          // Marking as a blocker since there was an error and so
+          // we can prevent continued requests when this function is run
+          console.log(error);
+          isBlocked = true;
+          return isBlocked;
+        }
+      }
+
+      return isBlocked !== undefined ? isBlocked : await tryRequest();
+    }
+
+    setIsAdblock(checkAdBlocker());
   }, [])
 
   return (
