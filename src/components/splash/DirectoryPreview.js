fb8bb0d93edc4136b676b7d4a87a755f95ac55df
jeffrey-fabian <jfabian@rsglab.com>
2020-10-01 18:22:36 -0400

    increase directory preview time

--- src/components/splash/DirectoryPreview.js
+++ src/components/splash/DirectoryPreview.js
@@ -226,7 +226,7 @@ export default function DirectoryPreview({ context }) {
             navButtonsAlwaysVisible
             autoPlay={!context.isMobile}
             animation="slide"
-            interval={6000}
+            interval={9000}
           >
             {data.allRecruitingResource.nodes.map((node, index) => (
               <div className={classes.item}>
